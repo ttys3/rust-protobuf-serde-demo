6a15f5cdb2a4205d217f9f44425bef3c246a5c3f
ttyS3 <ttys3.rust@gmail.com>
2022-08-23 20:30:28 +0800

    style: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,19 @@ fn main() {
 // https://doc.rust-lang.org/cargo/reference/environment-variables.html
 pub mod items {
     // include!(concat!(env!("OUT_DIR"), "/snazzy.items.rs"));
-    include!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/proto/snazzy.items.rs"));
+    include!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/src/proto/snazzy.items.rs"
+    ));
 }
 
 pub fn create_large_shirt(color: String) -> items::Shirt {
     let mut shirt = items::Shirt::default();
     shirt.color = color;
-    shirt.ts = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as i64;
+    shirt.ts = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_millis() as i64;
     shirt.set_size(items::shirt::Size::Large);
     shirt
 }
